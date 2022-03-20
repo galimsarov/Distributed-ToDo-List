ee79b082c90462d728f389fe04e0b1eaec2b1fd8
Evgenii <e.galimsarov@gmail.com>
2022-03-20 10:52:50 +0300

    toDoItem added

--- force-app/main/default/lwc/toDoItem/toDoItem.js
+++ force-app/main/default/lwc/toDoItem/toDoItem.js
@@ -1,5 +1,18 @@
 import { LightningElement, api } from 'lwc';
 
 export default class ToDoItem extends LightningElement {
-    @api todo
+    @api todoname
+    @api todostatus
+
+    get isInProgress() {
+        return this.todostatus === 'In Progress';
+    }
+
+    get isReadyToTake () {
+        return this.todostatus === 'Ready to Take';
+    }
+
+    get isDone () {
+        return this.todostatus === 'Done';
+    }
 }
