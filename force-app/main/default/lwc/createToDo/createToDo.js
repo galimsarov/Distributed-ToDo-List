cd64082271446d754927bca2292805b967c5bc66
Евгений Галимсаров <address@email.com>
2022-03-22 15:21:53 +0500

    Used record types

--- force-app/main/default/lwc/createToDo/createToDo.js
+++ force-app/main/default/lwc/createToDo/createToDo.js
@@ -1,15 +1,20 @@
 import { LightningElement, wire } from 'lwc';
 import NAME_FIELD from '@salesforce/schema/ToDo__c.Name';
 import STATUS_FIELD from '@salesforce/schema/ToDo__c.Status__c';
+import RECORD_TYPE_ID_FIELD from '@salesforce/schema/ToDo__c.RecordTypeId';
 import TODO_OBJECT from '@salesforce/schema/ToDo__c';
 import { createRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { reduceErrors } from 'c/ldsUtils';
+// importing to get the object info 
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 
 export default class CreateToDo extends LightningElement {
     toDoId;
     name = '';
     statusValue = 'Ready to Take';
+    selectedRecordTypeValue;
+    recordTypeOptions = [];
     
     handleNameChange(event) {
         this.toDoId = undefined;
@@ -28,32 +33,74 @@ export default class CreateToDo extends LightningElement {
             { label: 'Done', value: 'Done' },
         ];
     }
+    
+    // object info using wire service
+    @wire(getObjectInfo, { objectApiName: TODO_OBJECT })
+    toDoObjectInfo({data, error}) {
+        if (data) {
+            let optionsValues = [];
+            // map of record type Info
+            const rtInfos = data.recordTypeInfos;
+   
+            // getting map values
+            let rtValues = Object.values(rtInfos);
+   
+            for (let i = 0; i < rtValues.length; i++) {
+                if (rtValues[i].name !== 'Master') {
+                    optionsValues.push({
+                        label: rtValues[i].name,
+                        value: rtValues[i].recordTypeId
+                    })
+                }
+            }
+            
+            this.recordTypeOptions = optionsValues;
+        } else if (error) {
+            window.console.log('Error ===> '+JSON.stringify(error));
+        }
+    }
+    
+    // Handling on change value
+    handleRecordTypeChange(event) {
+        this.selectedRecordTypeValue = event.detail.value;
+    }
 
     createTodo() {
-        const fields = {};
-        fields[NAME_FIELD.fieldApiName] = this.name;
-        fields[STATUS_FIELD.fieldApiName] = this.statusValue;
-        const recordInput = { apiName: TODO_OBJECT.objectApiName, fields };
-        createRecord(recordInput)
-        .then((toDo) => {
-            this.toDoId = toDo.id;
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Success',
-                    message: 'ToDo created',
-                    variant: 'success'
-                })
-            );
-            this.dispatchEvent(new CustomEvent('refreshtodolist'));
-        })
-        .catch((error) => {
+        if (this.name === '') {
             this.dispatchEvent(
                 new ShowToastEvent({
                     title: 'Error creating record',
-                    message: reduceErrors(error).join(', '),
+                    message: 'Please, enter name for ToDo',
                     variant: 'error'
                 })
-            )
-        });
+            );
+        } else {
+            const fields = {};
+            fields[NAME_FIELD.fieldApiName] = this.name;
+            fields[STATUS_FIELD.fieldApiName] = this.statusValue;
+            fields[RECORD_TYPE_ID_FIELD.fieldApiName] = this.selectedRecordTypeValue;
+            const recordInput = { apiName: TODO_OBJECT.objectApiName, fields };
+            createRecord(recordInput)
+            .then((toDo) => {
+                this.toDoId = toDo.id;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'ToDo created',
+                        variant: 'success'
+                    })
+                );
+                this.dispatchEvent(new CustomEvent('refreshtodolist'));
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error creating record',
+                        message: reduceErrors(error).join(', '),
+                        variant: 'error'
+                    })
+                )
+            });
+        }
     }
 }
--- force-app/main/default/lwc/toDoItem/toDoItem.js
+++ force-app/main/default/lwc/toDoItem/toDoItem.js
@@ -1,12 +1,20 @@
-import { LightningElement, api} from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import getSubToDos from '@salesforce/apex/SubToDoController.getAllSubToDos';
+// importing to get the object info 
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import TODO_OBJECT from '@salesforce/schema/ToDo__c';
 
 export default class ToDoItem extends LightningElement {
     @api todoname
     @api todostatus
     @api todoid
-
+    @api recordtypeid
+    
     subToDos;
+    
+    isToday = false;
+    isLater = false;
+    isTomorrow = false;
 
     connectedCallback() {
         getSubToDos({toDoId : this.todoid})
@@ -25,5 +33,36 @@ export default class ToDoItem extends LightningElement {
 
     get isDone () {
         return this.todostatus === 'Done';
+    }
+
+    // object info using wire service
+    @wire(getObjectInfo, { objectApiName: TODO_OBJECT })
+    toDoObjectInfo({data, error}) {
+        if (data) {
+            // map of record type Info
+            const rtInfos = data.recordTypeInfos;
+           
+            // getting map values
+            let rtValues = Object.values(rtInfos);
+           
+            for (let i = 0; i < rtValues.length; i++) {
+                if (rtValues[i].name !== 'Master') {
+                    if ((rtValues[i].name === 'Today') && (rtValues[i].recordTypeId === this.recordtypeid)) {
+                        this.isToday = true;
+                        break;
+                    }
+                    if ((rtValues[i].name === 'Later') && (rtValues[i].recordTypeId === this.recordtypeid)) {
+                        this.isLater = true;
+                        break;
+                    }
+                    if ((rtValues[i].name === 'Tomorrow') && (rtValues[i].recordTypeId === this.recordtypeid)) {
+                        this.isTomorrow = true;
+                        break;
+                    }
+                }
+            }
+        } else if (error) {
+            window.console.log('Error ===> '+JSON.stringify(error));
+        }
     }
 }
